7e31933b6acafee4703733c0fffb5c3bf7a843ad
Carlos Torres <carlos.torres@trademate.tech>
2022-11-27 19:35:46 -0500

    apriori 13

--- features/web/step_definitions/step_login.js
+++ features/web/step_definitions/step_login.js
@@ -1,5 +1,6 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const expect = require('chai').expect;
+const { faker } = require('@faker-js/faker');
 
 When('I enter email {kraken-string}', async function(email){
   let element = await this.driver.$('input[placeholder="Email Address"]');
@@ -29,4 +30,10 @@ When('I enter password {kraken-string} V4.44', async function(password){
 When('I click sign in V4.44', async function(){
   let element = await this.driver.$('button[class="login gh-btn gh-btn-login gh-btn-block gh-btn-icon js-login-button ember-view"]');
   return await element.click();
+})
+
+When('I enter email random email to the limit with faker', async function(){
+  let element = await this.driver.$('input[placeholder="Email Address"]');
+  let title = faker.lorem.slug(50)
+  return await element.setValue(title);
 })
